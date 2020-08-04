e8665a1d098c7fc44e5cd87a7fd10b091e643442
Mouhanad alaskari <mouhanad.alaskari@sheenvalue.com>
2020-08-04 15:32:59 +0300

    Updates

---
+++ precache-manifest.91d18bb9241fe7495ba73aef26e495eb.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8471d4d6329b95c89655e1b5cb4afb54",
+    "url": "/ventrue-demo/index.html"
+  },
+  {
+    "revision": "5a8acfc7d6b084fd10be",
+    "url": "/ventrue-demo/static/css/2.5a8905d2.chunk.css"
+  },
+  {
+    "revision": "55abcfffdc139e3d5d65",
+    "url": "/ventrue-demo/static/css/main.fe15db23.chunk.css"
+  },
+  {
+    "revision": "5a8acfc7d6b084fd10be",
+    "url": "/ventrue-demo/static/js/2.156d96db.chunk.js"
+  },
+  {
+    "revision": "55abcfffdc139e3d5d65",
+    "url": "/ventrue-demo/static/js/main.2f5b70ca.chunk.js"
+  },
+  {
+    "revision": "b153eedcc7e7f2f8b717",
+    "url": "/ventrue-demo/static/js/runtime-main.1b280c49.js"
+  },
+  {
+    "revision": "6b73a3180a8026aa1d4d8686e5287f17",
+    "url": "/ventrue-demo/static/media/10.6b73a318.jpg"
+  },
+  {
+    "revision": "1ff711d2829a368814dbb9c92a0888de",
+    "url": "/ventrue-demo/static/media/7.1ff711d2.jpg"
+  },
+  {
+    "revision": "fdf0aed1a736f875cfda350808d86203",
+    "url": "/ventrue-demo/static/media/Soft-Skills.fdf0aed1.png"
+  },
+  {
+    "revision": "fb4a474a77a677fd5edde91599ffeebd",
+    "url": "/ventrue-demo/static/media/Thematic-Topics.fb4a474a.png"
+  },
+  {
+    "revision": "af68ef7076758e2a8b27758c5fe91793",
+    "url": "/ventrue-demo/static/media/banner1.af68ef70.jpg"
+  },
+  {
+    "revision": "2de41e5b927e1790b1c5b9ee1f173e45",
+    "url": "/ventrue-demo/static/media/banner2.2de41e5b.jpg"
+  },
+  {
+    "revision": "934b52f7f100261d7e634486758dfe9d",
+    "url": "/ventrue-demo/static/media/footer-shape.934b52f7.png"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/ventrue-demo/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/ventrue-demo/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "8518ca8b5e21ad1da00140e5745804bf",
+    "url": "/ventrue-demo/static/media/map.8518ca8b.png"
+  },
+  {
+    "revision": "b8654bd430c3e491d18f570aac7a8ee1",
+    "url": "/ventrue-demo/static/media/oil.b8654bd4.jpg"
+  },
+  {
+    "revision": "cc3fd4a6d5f58bc3a3cacd9f23eb9bff",
+    "url": "/ventrue-demo/static/media/overview1.cc3fd4a6.png"
+  },
+  {
+    "revision": "66d1750d510158e3d67dfe13ac9b2730",
+    "url": "/ventrue-demo/static/media/overview2.66d1750d.png"
+  },
+  {
+    "revision": "629ad8ebd6581fc2e1ea2147d8ab559e",
+    "url": "/ventrue-demo/static/media/overview3.629ad8eb.png"
+  },
+  {
+    "revision": "04e9d8ff5f6b96dc97822bbb23cb5ce5",
+    "url": "/ventrue-demo/static/media/overview4.04e9d8ff.png"
+  },
+  {
+    "revision": "21feef2f0e0c3173b85c3d9fc5343c50",
+    "url": "/ventrue-demo/static/media/saas1.21feef2f.png"
+  },
+  {
+    "revision": "212037acab60d4f6a5184b06400ebb72",
+    "url": "/ventrue-demo/static/media/venture.212037ac.png"
+  },
+  {
+    "revision": "2bcf975a1642e308e476617ac2532cab",
+    "url": "/ventrue-demo/static/media/venture2.2bcf975a.png"
+  }
+]);
